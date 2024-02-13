938a408b9c743c61ca824248360d50ef55492217
01FE22BCA207 <156279910+01FE22BCA207@users.noreply.github.com>
2024-02-13 10:08:46 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 public class Calculator{
     public float add(float num1, float num2){
       float addtion=num1+num2;
@@ -33,9 +33,11 @@ public class Calculator{
     }
         public static void main(String[] args) {
             System.out.print("Enter the 2 numbers:");
-            Scanner sc=new Scanner(System.in);
-            float num1=sc.nextFloat();
-            float num2=sc.nextFloat();
+          // Scanner sc=new Scanner(System.in);
+           // float num1=sc.nextFloat();
+          //  float num2=sc.nextFloat();
+            float num1=20;
+            float num2=10;
             Calculator c=new Calculator();
             System.out.println("The Addtion of two numbers:"+c.add(num1, num2));
             System.out.println("The Addtion of two numbers:"+c.sub(num1, num2));
@@ -43,7 +45,7 @@ public class Calculator{
             System.out.println("The Addtion of two numbers:"+c.div(num1, num2));
             System.out.println("The Square of number:"+c.squ(num1));
             System.out.println("The Cube of number:"+c.cube(num1));
-            sc.close();
+           // sc.close();
         }
 }
         
